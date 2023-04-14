02e308bf9ae6da4750141fd1385b50a975609b6a
heyapplemango <sojeong@a41.io>
2023-04-14 15:05:43 +0900

    fix: _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
+      <Head />
       <body>
-        <Head />
         <Main />
         <NextScript />
       </body>
